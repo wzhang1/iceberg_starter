0f3251dbb1472994e3dff6eac4d5689b6d5bc5df
wzhang1 <zhangvszhang@gmail.com>
2017-10-26 10:17:20 -0700

    Update create_files.sh

--- input/create_files.sh
+++ input/create_files.sh
@@ -1,5 +1,5 @@
-python json_to_png.py --json_input train.json --output train --label_file train.lst --is_test False
-python json_to_png.py --json_input test.json --output test --label_file test.lst --is_test True
+python json_to_jpg.py --json_input train.json --output train --label_file train.lst --is_test False
+python json_to_jpg.py --json_input test.json --output test --label_file test.lst --is_test True
 python split.py 
 python -u im2rec.py --resize 128 --quality 99 --num-thread 20 val ./
 python -u im2rec.py --resize 128 --quality 99 --num-thread 20 train ./
