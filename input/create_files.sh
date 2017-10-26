7a8553f83a127a261af9bac3d8f7696bbeacdded
wzhang1 <zhangvszhang@gmail.com>
2017-10-26 11:14:28 -0700

    Update create_files.sh
    need to avoid some name collision with the conversion tool

--- input/create_files.sh
+++ input/create_files.sh
@@ -1,7 +1,7 @@
 python json_to_jpg.py --json_input train.json --output train --label_file train.lst --is_test False
-python json_to_jpg.py --json_input test.json --output test --label_file test.lst --is_test True
+python json_to_jpg.py --json_input test.json --output test --label_file test_list.lst --is_test True
 python split.py 
 python -u im2rec.py --resize 128 --quality 99 --num-thread 20 val ./
-python -u im2rec.py --resize 128 --quality 99 --num-thread 20 train ./
-python -u im2rec.py --resize 128 --quality 99 --num-thread 20 --list test.lst ./
+python -u im2rec.py --resize 128 --quality 99 --num-thread 20  train_split ./
+python -u im2rec.py --resize 128 --quality 99 --num-thread 20 test_list ./
 
